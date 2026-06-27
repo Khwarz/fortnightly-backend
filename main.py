b7fcb56f79e6d4a36e0d12f0ab5235db14189836
Kino Cooler <kinocooler@gmail.com>
2026-06-27 17:30:20 +0100

    Added get article endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -14,6 +15,17 @@ fake_db = [
         "published_at": "2026-06-24 20:00:00",
         "author": "John Doe",
         "read_duration": 8
+    },
+    {
+        "id": 2,
+        "title": "The Architecture of FastAPI",
+        "excerpt": "As cities grow louder, a new generation of architects "
+        "is prioritizing acoustic serenity and visual void. We examine the "
+        "structural shift toward environments that demand nothing but your "
+        "attention.",
+        "published_at": "2026-06-27 20:00:00",
+        "author": "Jane Doe",
+        "read_duration": 8
     }
 ]
 
@@ -21,3 +33,15 @@ fake_db = [
 @app.get("/articles")
 def list_articles():
     return fake_db
+
+
+@app.get("/articles/{article_id}")
+def get_article(article_id: int): # pour recuperer les articles sous forme d'un entier
+    for item in fake_db:
+        if item["id"] == article_id:
+            return item
+        
+    raise HTTPException(
+        status_code=404,
+        detail="Article introuvable"
+    )
